d0cc6d00021bb6d8090cd5994545d726cf04c532
hyori100 <ag497752@naver.com>
2024-02-01 00:32:21 +0900

    영화사이트 예제앱 생성 및 router설치

---
+++ practice-app/src/components/Movie.js
@@ -0,0 +1,13 @@
+
+function Movie({medium_cover_image, title, summary, genres}){
+    return <div>
+    <img src={medium_cover_image} alt={title}></img>
+    <h2>{title}</h2>
+    <p>{summary}</p>
+    <ul>
+      {genres.map(g => <li key={g}>{g}</li>)}
+    </ul>
+  </div>
+}
+
+export default Movie;
